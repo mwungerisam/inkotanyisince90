0c039834b396d81f6369cbe0a208d1c123453256
mwungerisam <mwungerisam63@gmail.com>
2026-08-06 00:26:15 +0200

    Add explicit head.tsx favicon links for browser tab icon

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/favicon.ico" />
+      <meta name="theme-color" content="#000000" />
+    </>
+  );
+}
