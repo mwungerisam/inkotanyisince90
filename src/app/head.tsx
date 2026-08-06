17c927a830790ec5dc8da6c35b3fa8f3c4cf0550
mwungerisam <mwungerisam63@gmail.com>
2026-08-06 02:09:00 +0200

    Add professional favicon PNG assets and explicit icon links

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,9 +1,10 @@
 export default function Head() {
   return (
     <>
-      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-      <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-      <link rel="apple-touch-icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/favicon-32x32.png" />
       <meta name="theme-color" content="#000000" />
     </>
   );
